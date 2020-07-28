b6e0f952e42d551ea077b0228b15b41d6e629db5
Andrés Ruiz <franklin.ruiz@epn.edu.ec>
2020-07-28 01:27:17 -0500

    User desing (solved problem)

--- app/src/main/java/com/projectmoviles/myapplication/User/UAgenActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/User/UAgenActivity.kt
@@ -1,5 +1,6 @@
 package com.projectmoviles.myapplication.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.projectmoviles.myapplication.R
@@ -9,4 +10,10 @@ class UAgenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_u_agen)
     }
+    btn_homeusuario..setOnClickListener{
+        var intent = Intent(this, UsuarioActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/projectmoviles/myapplication/User/UAgenAgregarActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/User/UAgenAgregarActivity.kt
@@ -1,5 +1,6 @@
 package com.projectmoviles.myapplication.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.projectmoviles.myapplication.R
@@ -8,5 +9,24 @@ class UAgenAgregarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_u_agen_agregar)
+    }
+    btn_check_horario.setOnClickListener{
+        Toast.makeText(getApplicationContext(),
+            "Horario Registrado", Toast.LENGTH_SHORT).show()
+        var intent = Intent(this, UAgenActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    btn_check_horario.setOnClickListener{
+        Toast.makeText(getApplicationContext(),
+            "Horario no registrado", Toast.LENGTH_SHORT).show()
+        var intent = Intent(this, UAgenActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    btn_atrasagendausuario.setOnClickListener{
+        var intent = Intent(this, UAgenActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
