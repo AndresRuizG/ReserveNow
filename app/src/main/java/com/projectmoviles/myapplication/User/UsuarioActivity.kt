c4fd96e1cd325d92201364b1283f1eb78af7a8b3
Joel Villacis <kevin.villacis01@epn.edu.ec>
2020-08-10 13:38:07 -0500

    Fondo usuario cambio de estructura y colores

--- app/src/main/java/com/projectmoviles/myapplication/User/UsuarioActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/User/UsuarioActivity.kt
@@ -1,17 +1,34 @@
 package com.projectmoviles.myapplication.User
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.projectmoviles.myapplication.LoginActivity
 import com.projectmoviles.myapplication.R
-import com.projectmoviles.myapplication.admin.AdminActivity
 import kotlinx.android.synthetic.main.activity_usuario.*
 
+
 class UsuarioActivity : AppCompatActivity() {
+    var drawerLayout: DrawerLayout? = null
+    var navigationView: NavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usuario)
+
+        drawerLayout = findViewById(R.id.drawer_menu);
+        navigationView = findViewById(R.id.nav_view);
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        toggle.syncState();
+/*
         btn_agendarusuario.setOnClickListener{
             var intent = Intent(this, AgendaUsuarioActivity::class.java)
             startActivity(intent)
@@ -33,6 +50,6 @@ class UsuarioActivity : AppCompatActivity() {
             var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }*/
     }
 }
