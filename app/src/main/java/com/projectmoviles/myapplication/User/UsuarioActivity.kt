97e878e6c129a763b0d1f90328612373698f27d3
Joel Villacis <kevin.villacis01@epn.edu.ec>
2020-08-10 17:16:32 -0500

    Menu lateral acabado

--- app/src/main/java/com/projectmoviles/myapplication/User/UsuarioActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/User/UsuarioActivity.kt
@@ -1,55 +1,60 @@
 package com.projectmoviles.myapplication.User
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import com.projectmoviles.myapplication.LoginActivity
 import com.projectmoviles.myapplication.R
 import kotlinx.android.synthetic.main.activity_usuario.*
 
 
-class UsuarioActivity : AppCompatActivity() {
+class UsuarioActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     var drawerLayout: DrawerLayout? = null
     var navigationView: NavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usuario)
 
-        drawerLayout = findViewById(R.id.drawer_menu);
-        navigationView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_menu)
+        navigationView = findViewById(R.id.nav_view)
 
-        val toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        toggle.syncState();
-/*
-        btn_agendarusuario.setOnClickListener{
-            var intent = Intent(this, AgendaUsuarioActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        btn_historialusuario.setOnClickListener{
-            var intent = Intent(this, HistorialUsuarioActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        btn_mebresiausuario.setOnClickListener{
+        navigationView?.bringToFront()
+        val toggle =
+            ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+            )
+        toggle.syncState()
 
-            var intent = Intent(this, MembresiaUsuarioActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        buttonConfig.setOnClickListener{
+        navigationView?.setNavigationItemSelectedListener(this);
 
-            var intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }*/
     }
+
+    override fun onBackPressed() {
+        if(drawerLayout!!.isDrawerOpen(GravityCompat.START)){
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        /*switch (menuItem.getItemId()){
+            case R.id.nav_perfil:
+            TODO
+
+            case R.id.nav_salir:
+            TODO
+        }*/
+        return true;
+    }
+
+
 }
