cbd0b2d2a4955dd67b8bbe7874a40da2359241b1
Jorge Quilumba <jorge.quilumba@uio.intelectus-system.com>
2020-09-09 10:50:42 -0500

    -Autenticacion con proveedor Google -Archivo SHA1

--- app/src/main/java/com/projectmoviles/myapplication/LoginActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/LoginActivity.kt
@@ -1,23 +1,30 @@
 package com.projectmoviles.myapplication
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.projectmoviles.myapplication.User.ProviderType
 import com.projectmoviles.myapplication.User.UsuarioActivity
 import com.projectmoviles.myapplication.admin.AdminActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private  val GOOGLE_SING_IN = 100
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance();
         /*btnInicio.setOnClickListener {
             if(editTextEmailAddress.text.toString().equals("jorge") and editTextPassword.text.toString().equals("12345678")){
                 var intent = Intent(this, UsuarioActivity::class.java)
@@ -54,8 +61,18 @@ class LoginActivity : AppCompatActivity() {
         bundle.putString("Message", "Integracion de firebase completa")
         analytics.logEvent("InitScreen", bundle)
         inicioSesion()
+        //inicioSesionGoogle()
 
     }
+   /* private fun inicioSesionGoogle(){
+        val prefs =getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val correo = prefs.getString("correo", null)
+        val provider = prefs.getString("provider", null)
+        if(correo !=null && provider !=null){
+            showHomeUsuario(correo, ProviderType.valueOf(provider))
+        }
+
+    }*/
     fun inicioSesion(){
         title ="Autenticacion"
         btnInicio.setOnClickListener {
@@ -63,7 +80,7 @@ class LoginActivity : AppCompatActivity() {
             CONTRASENIA = editTextPassword.text.toString();
             if (!CORREO.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(CORREO).matches() &&
                 !CONTRASENIA.isEmpty() && CONTRASENIA.length >= 8) {
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(CORREO, CONTRASENIA).addOnCompleteListener{
+                auth.signInWithEmailAndPassword(CORREO, CONTRASENIA).addOnCompleteListener{
                     if(it.isSuccessful){
                         showHomeUsuario()
                         Toast.makeText(this, MENSAJE_BIENVEINDA, Toast.LENGTH_SHORT).show()
@@ -83,6 +100,15 @@ class LoginActivity : AppCompatActivity() {
             var registrarIntent = Intent(this, RegistraseActivity::class.java)
             startActivity(registrarIntent)
         }
+        btnGoogle.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SING_IN)
+        }
 
     }
 
@@ -100,4 +126,29 @@ class LoginActivity : AppCompatActivity() {
         val homeIntent = Intent(this, UsuarioActivity::class.java)
         startActivity(homeIntent)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==GOOGLE_SING_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if(account!= null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+                    auth.signInWithCredential(credential).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHomeUsuario()
+                            Toast.makeText(this, MENSAJE_BIENVEINDA, Toast.LENGTH_SHORT).show()
+                        }else{
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException){
+                showAlert()
+            }
+
+        }
+    }
+
 }
--- app/src/main/java/com/projectmoviles/myapplication/RegistraseActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/RegistraseActivity.kt
@@ -4,70 +4,58 @@ import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
 import android.widget.Toast
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
-import com.projectmoviles.myapplication.User.ProviderType
-import com.projectmoviles.myapplication.User.UsuarioActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_registrarse.*
 
 class RegistraseActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
-        val analytics = FirebaseAnalytics.getInstance(this)
-        val bundle = Bundle()
-        bundle.putString("Message", "Integracion de firebase completa")
-        analytics.logEvent("InitScreen", bundle)
-        registrarUser()
+        auth = FirebaseAuth.getInstance();
+        btnRegistrarDatos.setOnClickListener {
+                NOMBRRE_USUARIO = editTextPersonName.text.toString();
+                val correo = editTextEmailAddress.getText().toString();
+                val contrasenia = editTextPassword.text.toString();
+                CONFIRMAR_CONTRASENIA= editTextPasswordConfirm.text.toString();
+                if (contrasenia.isEmpty() || contrasenia.length < 8) {
+                    editTextPassword.setError(MENSAJE_ERROR_CONTRASENIA)
+                    return@setOnClickListener
+                }
+                if(CONFIRMAR_CONTRASENIA!=contrasenia){
+                    editTextPasswordConfirm.setError(MENSAJE_ERROR_CONTRASENIAS_IGUALES);
+                    return@setOnClickListener
+                }
+            registroUsuario(correo, contrasenia)
+
+        }
+        btnInicioLogin.setOnClickListener {
+            var loginIntent =Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
-    fun registrarUser(){
-        title ="Registar usuario"
-        btnRegistrarDatos.setOnClickListener {
-            NOMBRRE_USUARIO = editTextPersonName.text.toString();
-            CORREO = editTextEmailAddress.getText().toString();
-            CONTRASENIA = editTextPassword.text.toString();
-            CONFIRMAR_CONTRASENIA= editTextPasswordConfirm.text.toString();
 
-            if (NOMBRRE_USUARIO.isNotEmpty() && !CORREO.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(CORREO).matches() &&
-                !CONTRASENIA.isNotEmpty() && CONTRASENIA.length >= 8 && CONTRASENIA==CONFIRMAR_CONTRASENIA) {
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(CORREO, CONTRASENIA).addOnCompleteListener{
-                    if(it.isSuccessful){
-                        showHomeUsuario(it.result?.user?.email ?: "", ProviderType.BASIC)
-                        Toast.makeText(this, MENSAJE_USUARIO_CREADO, Toast.LENGTH_SHORT).show()
-                    }else{
-                        showAlert()
-                    }
-                }
-                Toast.makeText(this, MENSAJE_BIENVEINDA, Toast.LENGTH_SHORT).show()
-            }
-            if (CORREO.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(CORREO).matches()) {
-                editTextEmailAddress.setError(MENSAJE_ERROR_CORREO)
-            }
-            if (CONTRASENIA.isEmpty() || CONTRASENIA.length < 8) {
-                editTextPassword.setError(MENSAJE_ERROR_CONTRASENIA)
-            }
-            if(CONFIRMAR_CONTRASENIA!=CONTRASENIA){
-                editTextPasswordConfirm.setError(MENSAJE_ERROR_CONTRASENIAS_IGUALES);
+    private fun registroUsuario(correo:String, contrasenia:String){
+        auth.createUserWithEmailAndPassword(correo, contrasenia)
+            .addOnCompleteListener(this){ task->
+            if(task.isSuccessful){
+                val user =auth.currentUser
+                Toast.makeText(baseContext, "Usuario agregado", Toast.LENGTH_SHORT).show()
+            }else{
+                showAlert()
             }
         }
     }
     fun showAlert(){
         val builder = AlertDialog.Builder(this)
+        builder.setIcon(R.drawable.ic_error)
         builder.setTitle("Error")
-        builder.setMessage("Error en la autenticacion de usuario")
+        builder.setMessage("Error en la creación de usuario")
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
-    }
-    fun showHomeUsuario(correo: String, contrasenia: ProviderType ){
-        val homeIntent = Intent(this, UsuarioActivity::class.java).apply {
-            putExtra("correo", correo)
-            putExtra("contrasenia", contrasenia.name)
-        }
-        startActivity(homeIntent)
     }
 }
