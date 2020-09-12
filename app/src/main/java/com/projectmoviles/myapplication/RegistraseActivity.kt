b0c88549180fd445dabcbd9bd9aa5e0d9c1feff6
Jorge Quilumba <jorge.quilumba@uio.intelectus-system.com>
2020-09-12 11:30:02 -0500

    -Autenticacion con Google (gmail) -Almacenamiento de usuarios en cloud Firestore -Constantes para los mensajes de exito y de error

--- app/src/main/java/com/projectmoviles/myapplication/RegistraseActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/RegistraseActivity.kt
@@ -15,10 +15,12 @@ import java.util.HashMap
 
 class RegistraseActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    lateinit var mDatabase: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
         auth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseFirestore.getInstance()
         btnRegistrarDatos.setOnClickListener {
             NOMBRRE_USUARIO = editTextPersonName.text.toString();
             CORREO = editTextTextEmailAddress.text.toString()
@@ -36,37 +38,62 @@ class RegistraseActivity : AppCompatActivity() {
             if (CONTRASENIA != CONFIRMAR_CONTRASENIA) {
                 Toast.makeText(this, MENSAJE_ERROR_CONTRASENIAS_IGUALES, Toast.LENGTH_LONG).show()
             }
-            /*if(!correo.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(correo).matches() &&
-                !pass.isEmpty() && pass.length >=8 && !confirPass.isEmpty() && pass==confirPass)
-                agregarUsuario(correo, pass)
-               Toast.makeText(this,"Se ha registrado exitosamente", Toast.LENGTH_SHORT).show()
-            }*/
-            registroUsuario(CORREO, CONTRASENIA)
-
-
+            if(!CORREO.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(CORREO).matches() &&
+                !CONTRASENIA.isEmpty() && CONTRASENIA.length >=8 && !CONFIRMAR_CONTRASENIA.isEmpty() && CONTRASENIA==CONFIRMAR_CONTRASENIA){
+                registroUsuario(CORREO, CONTRASENIA)
+                guardarUsuario(NOMBRRE_USUARIO, CORREO, CONTRASENIA)
+                limpiarCampos()
+                irALogin()
+            }
         }
         btnInicioLogin.setOnClickListener {
-            var loginIntent =Intent(this, LoginActivity::class.java)
-            startActivity(loginIntent)
+            irALogin()
         }
     }
 
+    fun guardarUsuario(NOMBRRE_USUARIO:String, CORREO:String, CONTRASENIA:String){
+
+        var map : HashMap<String, String>
+                = HashMap<String, String> ()
+        map.put("nombre", NOMBRRE_USUARIO);
+        map.put("correo", CORREO);
+        map.put("contrasenia", CONTRASENIA);
+        mDatabase.collection("usuarios").document()
+            .set(map)
+            .addOnSuccessListener {
+                Toast.makeText(this,MENSAJE_USUARIO_CREADO, Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,ERROR_CREAR_USUARIO, Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun limpiarCampos(){
+        editTextPersonName.setText("")
+        editTextTextEmailAddress.setText("")
+        editTextTextPassword.setText("")
+        editTextPasswordConfirm.setText("")
+    }
 
     fun registroUsuario(correo:String, pass:String){
         auth.createUserWithEmailAndPassword(correo,pass).addOnCompleteListener(this) { task ->
             if(task.isSuccessful){
                 val user =auth.currentUser
-                Toast.makeText(this,"Usuario guardado exitosamente", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,MENSAJE_USUARIO_CREADO, Toast.LENGTH_SHORT).show()
             }else{
                 showAlert()
             }
         }
     }
+    fun irALogin(){
+        var loginIntent =Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
     fun showAlert(){
         val builder = AlertDialog.Builder(this)
         builder.setIcon(R.drawable.ic_error)
         builder.setTitle("Error")
-        builder.setMessage("Error en la creación de usuario")
+        builder.setMessage(ERROR_CREAR_USUARIO)
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
