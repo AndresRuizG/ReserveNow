8da2565cf6f965c271396f8044d460711f94fac5
AndresRuizG <franklin.ruiz@epn.edu.ec>
2020-09-13 19:39:08 -0500

    Membresia Diaria

--- app/src/main/java/com/projectmoviles/myapplication/User/MembresiaDiariaActivity.kt
+++ app/src/main/java/com/projectmoviles/myapplication/User/MembresiaDiariaActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import com.projectmoviles.myapplication.R
 import kotlinx.android.synthetic.main.activity_membresia_diaria.*
 import kotlinx.android.synthetic.main.activity_membresia_usuario.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MembresiaDiariaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,17 @@ class MembresiaDiariaActivity : AppCompatActivity() {
         btnHomeMembresia.setOnClickListener {
             val intent= Intent(this, UsuarioActivity::class.java)
             startActivity(intent)
+        }
+        btnComprarDiario.setOnClickListener {
+            intent = Intent(this, MembresiaUsuarioActivity::class.java)
+            editTextTipoMembresia.setText("Diaria");
+            //deberia existir una variable para aumentar los dias o reducirlos.
+            editTextDiasVigentes.setText("1 Dia");
+            val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = sdf.format(Date())
+            //variables
+
+            editTextFechaVencimiento.setText(currentDate);
         }
     }
 }
